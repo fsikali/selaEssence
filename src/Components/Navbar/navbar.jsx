840a4adf3a503f401adbceae0977508c594bd5e2
fsikali <flemmingsikali499@gmail.com>
2024-04-20 21:39:16 +0300

    Addede: sela_essence folder

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react'  
+
+
 
 const navbar = () => {
   return (
         <div className="navbar-container">
              <ul>
-                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Store</a></li>
                   <li><a href="#">Home</a></li>
                   <li><a href="#">Home</a></li>
                   <li><a href="#">Home</a></li>
