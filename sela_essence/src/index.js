bf575dc7c0e06b13ed154e1edba24d4a03a4c6f8
fsikali <flemmingsikali499@gmail.com>
2024-04-23 23:01:48 +0300

    Deleted: comment

--- sela_essence/src/index.js
+++ sela_essence/src/index.js
@@ -11,7 +11,6 @@ root.render(
   </React.StrictMode>
 ); 
 
-//Check out this form
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
